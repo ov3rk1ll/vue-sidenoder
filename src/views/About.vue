5ab9c4749924149cbb5c26c6147c9a19f97225d2
ov3rk1ll <overkillerror@gmail.com>
2020-12-14 16:08:32 +0100

    about page with version

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      Vue Sideload <small class="text-muted">{{ version }}</small>
+    </h1>
+    <p class="lead">
+      Based on
+      <a href="https://github.com/whitewhidow/quest-sidenoder" target="_blank"
+        >SideNoder</a
+      >
+      by WhiteWhidow
+    </p>
+    <p>
+      To report a bug or issue, please click
+      <a href="https://github.com/ov3rk1ll/vue-sidenoder/issues" target="_blank"
+        >here</a
+      >.
+    </p>
+    <p>
+      Thanks to all the
+      <a
+        href="https://github.com/ov3rk1ll/vue-sidenoder/blob/main/CONTRIBUTORS.md"
+        target="_blank"
+        >contributors</a
+      >
+      to this app.
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  data: () => {
+    return {
+      version: process.env.PACKAGE_VERSION,
+    };
+  },
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const webpack = require("webpack");
+const fs = require("fs");
+const packageJson = fs.readFileSync("./package.json");
+const version = JSON.parse(packageJson).version || 0;
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          PACKAGE_VERSION: '"' + version + '"',
+        },
+      }),
+    ],
+  },
   pluginOptions: {
     electronBuilder: {
       // Use this to change the entrypoint of your app's main process
